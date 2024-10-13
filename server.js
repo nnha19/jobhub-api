9d93e8f1d9294ac77ab1ca4d2715da4cf4c10efd
nyi-ailytics <nyi.nyi@ailytics.ai>
2024-10-13 13:17:45 +0630

    connected to mongodb database

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tz76r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
